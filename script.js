08d939be407e622371211f216a87295e1c2d39ea
Brian Taylor <c.brian.taylor@gmail.com>
2023-04-23 20:30:01 -0700

    Update script.js
    full script

--- script.js
+++ script.js
@@ -1,22 +1,66 @@
-var number = document.getElementById("number");
-    countdown();
+let correctDisplay = document.getElementById("correct");
+let number = document.getElementById("number");
+let digits = document.getElementById("digits");
+let numcorrect = document.getElementById("numcorrect");
+let numincorrect = document.getElementById("numincorrect");
+let digit = 1;
+digits.textContent = digit.toString().concat("-back");
+let answer = -1;
+let scoreCorrect = 0;
+let scoreIncorrect = 0;
+let check = true;
+let start = false;
 
-    function countdown() {
-      var count = 10;
-      var timer = setInterval(function () {
-        if (count >= 0) {
-          number.innerHTML = count;
-          number.style.opacity = 1;
-          setTimeout(fadeout, 500);
-          count--;
+function checkNum(n) {
+    if (!check) {
+        if (n == answer) {
+            correctDisplay.textContent = "Correct";
+            scoreCorrect++;
+            numcorrect.textContent = scoreCorrect;
+
         } else {
-          clearInterval(timer);
-          number.innerHTML = "Done!";
+            correctDisplay.textContent = "Incorrect";
+            scoreIncorrect++;
+            numincorrect.textContent = scoreIncorrect;
+
         }
-      }, 1000);
+        start = false;
+        check = true;
     }
+}
 
-
-    function fadeout() {
-      number.style.opacity = '0';
+function countdown() {
+    if (!start) {
+        start = true;
+        correctDisplay.textContent = '';
+        var count = 8 + Math.floor(Math.random() * 7);
+        const randomIntArray = (n = 1) =>
+            Array.from({
+                    length: n
+                },
+                () => Math.floor(Math.random() * 10)
+            );
+        let numbers = randomIntArray(count+1);
+        answer = numbers[digit - 1];
+        var timer = setInterval(function() {
+            if (count >= 0) {
+                number.innerHTML = numbers[count];
+                number.style.opacity = 1;
+                setTimeout(fadeout, 500);
+                count--;
+            } else {
+                clearInterval(timer);
+                check = false;
+            }
+        }, 1000);
     }
+}
+
+function set_n(n) {
+    digit = n;
+    digits.textContent = n.toString().concat("-back");
+}
+
+function fadeout() {
+    number.style.opacity = '0';
+}
